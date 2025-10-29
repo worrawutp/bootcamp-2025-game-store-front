e144165cc78b8cdede7e1b89bb18f12def264609
Prakaykarn <prakaykarn.fa2546@gmail.com>
2025-10-29 17:27:29 +0700

    Add Discount in class price add price base and price discount. Include Calculate discount in main.js and set discount only here Escape from Duckov not all and discount can be calculated.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 import './reset.css'
 import './style.css'
+
+document.querySelectorAll('.card').forEach((card) => {
+    const price = parseFloat(card.dataset.price);
+    const discount = parseFloat(card.dataset.discount);
+
+    if (!isNaN(price) && !isNaN(discount)) {
+        const discountedPrice = price - (price * discount / 100);
+        const newPriceElement = card.querySelector('.price-discounted');
+        newPriceElement.textContent = `THB ${discountedPrice.toFixed(2)}`;
+    }
+});
