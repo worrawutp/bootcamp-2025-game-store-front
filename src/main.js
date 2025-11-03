56fd3ccbfd4b1fdcaf8a8f0a3fdeadbf346f1401
AKASUKA <kasubaranime@gmail.com>
2025-11-03 18:22:36 +0700

    Move the discount calculation code to the discount file.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,4 @@
 import './reset.css'
 import './style.css'
-
-// const price =  412.17;
-// const discount = 12 ;
-
-// const discount_sale = price * (discount /100);
-// const sale = price - discount_sale;
-
-// console.log(sale)
-
-const pricedisplay = document.getElementById('price');
-const dicountdisplay = document.getElementById('discount');
-const sale  = document.getElementById('discount_sale');
-
-const price = parseFloat(pricedisplay.textContent);
-const discount = parseInt(dicountdisplay.textContent)
-
-const totalPrice = price *(discount /100);
-const finalPrice =  price - totalPrice;
-sale.textContent = finalPrice.toFixed(2);
+import './addtag'
+import './discount'
