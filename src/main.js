1acbdc52770453fae8e4eafa087cb7878b12c650
Prakaykarn <prakaykarn.fa2546@gmail.com>
2025-11-04 21:27:07 +0700

    Delete folder game-list add DOM in main.js show .csv

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './reset.css'
 import './style.css'
+import rawData from "./db/game-list.csv?raw";
 
 document.querySelectorAll('.card').forEach((card) => {
     const price = parseFloat(card.dataset.price);
@@ -11,3 +12,21 @@ document.querySelectorAll('.card').forEach((card) => {
         newPriceElement.textContent = `THB ${discountedPrice.toFixed(2)}`;
     }
 });
+
+let gamelist = document.getElementById('game-list'); // create <div = "game-list">
+let gameData = rawData.split("\n")
+gameData.shift(); 
+
+gameData.forEach((line) => {
+    let data = line.split(",")
+    let gameInfo = {
+        title: data[0],
+        category: data[1],
+        base_price: data[2],
+        discount_percent: data[3],
+        first_run : data[4],
+    }
+    console.log(gameInfo);
+    let gamecared = `<div>${line}</div>`
+    gamelist.insertAdjacentHTML('beforeend', gamecared);
+})
