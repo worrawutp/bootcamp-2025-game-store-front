2e73c4aacafe13a26dbcf7009151c7f07cc7d93b
Aekkarat12 <66001308111@udru.ac.th>
2025-11-09 00:02:48 +0700

    delete head array & behind array

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import rawData from "./db/game-list.csv?raw"
 let gameList = document.getElementById("game-list")
 let gameData = rawData.split("\n")
 
+gameData.shift()
+gameData.pop()
+console.log(gameData)
+
 gameData.forEach((line) => {
     let gameCard = `<div>${line}</div>`
     gameList.insertAdjacentHTML('beforeend', gameCard)
