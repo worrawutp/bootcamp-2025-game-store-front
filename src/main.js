5dcbe050166ead56b203e5c68516ee5dfc16d48f
Theerawit Waithayawan <pm.theerawit@gmail.com>
2025-11-10 21:00:47 +0700

    Refactor price formatting logic in calculateDiscountedPrice function

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
 import './reset.css'
 import './style.css'
+
+function calculateDiscountedPrice(price) {
+  if (price <= 0) return "Free";
+  return "THB " + (price % 1 === 0 ? price : price.toFixed(2));
+}
