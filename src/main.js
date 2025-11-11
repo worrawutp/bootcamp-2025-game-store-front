95b59e4e675f5ff238d9adfa368d44a7de2ba5e6
Theerawit Waithayawan <pm.theerawit@gmail.com>
2025-11-11 20:05:11 +0700

    Add num function to parse values and handle NaN cases

--- src/main.js
+++ src/main.js
@@ -5,3 +5,11 @@ function calculateDiscountedPrice(price) {
   if (price <= 0) return "Free";
   return "THB " + (price % 1 === 0 ? price : price.toFixed(2));
 }
+
+function num(value){
+  const num = parseFloat(value);
+  return insNaN(num) ? 0 : num;
+}
+
+
+
