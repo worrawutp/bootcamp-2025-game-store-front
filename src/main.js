3f96689caaafce0996b10741ba3c84627933f7e0
pwoot <worrawutp@gmail.com>
2025-12-09 20:13:57 +0700

    Able to show modal for game create form (#31)
    * Able to show modal for game create form
    
    - Use popover API to control modal and popover create form
    - Handle hiding popover form after received fetch response
    - Handle adding delete card button on the new game card
    - Note that we need to refactor on code duplication of the card DOM
    
    * Clear input value of new game form after submit new game
    
    * Use generic name for game form element
    
    - We will use generic name, game-form, for the form element so that we
      can reuse the form modal for both create and update game
    
    * Able to show edit game form with game value
    
    * Separate new-game-form and edit-game-form
    
    so that we can used javascript to manipulate input tag and submit button
    separately
    
    * Extract delete game and edit game functions into its own files
    
    - Update fetch request (patch) data to send gameInfo in json format to
      /games/:id endpoint to update the game record
    
    * Update game data on the card and then close popover modal
    
    * Correctly assign event listener for all deleted button

--- src/main.js
+++ src/main.js
@@ -2,68 +2,51 @@ import './reset.css'
 import './style.css'
 
 import getGames from "./gameDataService.js"
+import { submitDeleteGameCard } from './delete_game.js'
+import { editGameCard } from './edit_game.js'
 
 let gameListDom = document.getElementById("game-list")
 let rawData = await getGames()
-console.log(rawData)
 
 if (rawData) {
   rawData.forEach(gameInfo => {
-      let gameCardDom = `
-        <div class="card" id="card-${gameInfo.id}">
-            <div class="photo">
-              <img src="src/asset/${gameInfo.photoName}.jpeg" alt="${gameInfo.title}">
-            </div>
-            <div class="card-info">
-              <div class="category">${gameInfo.category}</div>
-              <div class="title">${gameInfo.title}</div>
-              <div class="price">THB ${gameInfo.base_price}</div>
-            </div>
-            <button data-game-id="${gameInfo.id}">
-              <?xml version="1.0" encoding="utf-8"?>
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M3 6H21M5 6V20C5 21.1046 5.89543 22 7 22H17C18.1046 22 19 21.1046 19 20V6M8 6V4C8 2.89543 8.89543 2 10 2H14C15.1046 2 16 2.89543 16 4V6" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M14 11V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M10 11V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
-            </button>
-          </div> <!-- card-->
-      `
-      gameListDom.insertAdjacentHTML('beforeend', gameCardDom)
+    let gameCardDom = `
+        <div class="card" id="card-${gameInfo.id}" data-game-info='${JSON.stringify(gameInfo)}'>
+          <div class="photo">
+            <img src="src/asset/${gameInfo.photoName}.jpeg" alt="${gameInfo.title}">
+          </div>
+          <div class="card-info">
+            <div class="category">${gameInfo.category}</div>
+            <div class="title">${gameInfo.title}</div>
+            <div class="price">THB ${gameInfo.base_price}</div>
+          </div>
 
-      // Add event listener for delete card button
-      let newCard = gameListDom.lastElementChild
-      let newCardDeleteBtn = newCard.querySelector("button")
-      newCardDeleteBtn.addEventListener("click", submitDeleteGameCard)
-    })
+          <button id="btn-delete" data-game-id="${gameInfo.id}" class="btn-delete">
+            <?xml version="1.0" encoding="utf-8"?>
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 6H21M5 6V20C5 21.1046 5.89543 22 7 22H17C18.1046 22 19 21.1046 19 20V6M8 6V4C8 2.89543 8.89543 2 10 2H14C15.1046 2 16 2.89543 16 4V6" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M14 11V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M10 11V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </button>
+
+          <button id="btn-edit-${gameInfo.id}" data-game-id="${gameInfo.id}" class="btn-edit">
+            Edit
+          </button> 
+        </div> <!-- card-->
+    `
+    gameListDom.insertAdjacentHTML('beforeend', gameCardDom)
+
+    // Add event listener for delete card button
+    let cardDeleteBtnList = document.querySelectorAll("#btn-delete")
+    cardDeleteBtnList.forEach(btn => btn.addEventListener("click", submitDeleteGameCard))
+
+    let cardEditBtn = document.getElementById(`btn-edit-${gameInfo.id}`)
+    cardEditBtn.addEventListener("click", editGameCard)
+  })
 
 } else {
   let errorElement = document.getElementById("error") 
   errorElement.insertAdjacentHTML(`beforeend`, `<p>Cannot connect to Game Store database</p>`)
 }
 
-function submitDeleteGameCard(event) {
-  const userConfirmed = confirm("Are you sure?")
-
-  if(userConfirmed) {
-    let trashBtn = event.currentTarget
-    let gameId = trashBtn.dataset.gameId
-    let url = `http://localhost:3000/games/${gameId}`
-
-    fetch(url, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-    }).then(resp => {
-      if(resp.ok) {
-        return resp.json()
-      }
-    }).then(data => {
-      const gameId = data.id
-      const deletedCard = document.getElementById(`card-${gameId}`)
-      deletedCard.remove()
-    })
-  }
-}
--- src/new_game.js
+++ src/new_game.js
@@ -1,4 +1,7 @@
-const btnSubmit = document.getElementById("btn-submit")
+import { submitDeleteGameCard } from "./delete_game"
+
+// submit new game
+const btnSubmit = document.getElementById("btn-submit-new-game")
 btnSubmit.addEventListener("click", function(event){
   event.preventDefault()
 
@@ -31,14 +34,9 @@ btnSubmit.addEventListener("click", function(event){
       }
     })
     .then((gameInfo) => {
-      console.log(gameInfo)
-
-      // TODO
-      // Read values from data 
-      // and then add a new game card to the dashboard with this data
       let gameListDom = document.getElementById("game-list")
       let gameCardDom = `
-          <div class="card">
+          <div class="card" id="card-${gameInfo.id}">
             <div class="photo">
               <img src="src/asset/${gameInfo.photoName}.jpeg" alt="Quartet">
             </div>
@@ -47,8 +45,33 @@ btnSubmit.addEventListener("click", function(event){
               <div class="title">${gameInfo.title}</div>
               <div class="price">THB ${gameInfo.base_price}</div>
             </div>
+
+            <button data-game-id="${gameInfo.id}">
+              <?xml version="1.0" encoding="utf-8"?>
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M3 6H21M5 6V20C5 21.1046 5.89543 22 7 22H17C18.1046 22 19 21.1046 19 20V6M8 6V4C8 2.89543 8.89543 2 10 2H14C15.1046 2 16 2.89543 16 4V6" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M14 11V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M10 11V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </button>
           </div> <!-- card-->
       `
       gameListDom.insertAdjacentHTML('beforeend', gameCardDom)
+
+      // Add event listener for delete card button
+      let newCard = gameListDom.lastElementChild
+      let newCardDeleteBtn = newCard.querySelector("button")
+      newCardDeleteBtn.addEventListener("click", submitDeleteGameCard)
+
+      const newGameForm = document.getElementById("new-game-form")
+      if(newGameForm != undefined) {
+        newGameForm.hidePopover()
+      }
+
+      // Clear form input values
+      title.value = ""
+      base_price.value = ""
+      discount_percent.value = ""
     })
 })
+
